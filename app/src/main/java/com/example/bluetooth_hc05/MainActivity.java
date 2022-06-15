9ad47123d14dd163c7ea210f4dca7124a76dcd68
thilina98 <thilina.avishka@gmail.com>
2022-06-15 15:12:11 +0530

    message sending is finished at a basic level. the commit before adding the drawer layout.

--- app/src/main/java/com/example/bluetooth_hc05/MainActivity.java
+++ app/src/main/java/com/example/bluetooth_hc05/MainActivity.java
@@ -1,28 +1,32 @@
 package com.example.bluetooth_hc05;
 
-import static android.os.SystemClock.sleep;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.pm.PackageManager;
+import android.icu.text.SymbolTable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
+import java.util.Arrays;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String relativeContacts[] = {"+94711261361"};
 
     Button btn_connect;
     TextView txt_data, txt_sts;
@@ -42,8 +46,10 @@ public class MainActivity extends AppCompatActivity {
     @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, PackageManager.PERMISSION_GRANTED);
+
         setContentView(R.layout.activity_main);
 
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
@@ -54,6 +60,27 @@ public class MainActivity extends AppCompatActivity {
 
         connect();
     }
+
+    /*
+    MESSAGE FORMAT
+    -------
+    message
+    -------
+    D,HHMMSS.SSS,TTTT.TTTT,GGGGG.GGGG
+
+    -------
+    content
+    -------
+    D - Accident Detection Flag
+    HHMMSS.SSS - Time
+    T - Latitude / G - Longitude
+
+    -------
+    example
+    -------
+    0,104534.000,7791.0381,06727.4434
+
+    */
 
     Handler handler = new Handler(new Handler.Callback() {
         @SuppressLint("HandlerLeak")
@@ -68,8 +95,37 @@ public class MainActivity extends AppCompatActivity {
                     break;
                 case STATE_MESSAGE_RECEIVED:
                     txt_data.setText((CharSequence) msg.obj);
+
+                    //=====================================================
+                    String dataString = (String) msg.obj;
+                    String dataArray[] = dataString.split(",");
+
+                    System.out.println(dataString);
+                    System.out.println(Arrays.toString(dataArray));
+
+//                    storeData(dataArray);    // to be implemented to store data in a database.
+
+                    String strTime = dataArray[1].substring(0,2)+"."+dataArray[1].substring(2,4);
+
+                    String version = "2.0";
+
+                    if (dataArray[0].equals("1")){
+                        SmsManager smsManagerSend = SmsManager.getDefault();
+                        String strMessage = version+"ACCIDENT DETECTED\n@"+strTime+"\n"+"view the location on google maps:\nhttps://www.google.com/maps/search/?api=1&query="+dataArray[2]+","+dataArray[3];
+                        System.out.println(strMessage);         // testing
+                        for (String contact: relativeContacts) {
+                            smsManagerSend.sendTextMessage(contact, null, strMessage, null, null);
+                        }
+                    }
+
+                    //=====================================================
+
+
                     break;
             }
+
+
+
             return false;
         }
     }){
@@ -142,7 +198,7 @@ public class MainActivity extends AppCompatActivity {
 
         public void run(){
 
-            byte[] buffer = new byte[10];
+            byte[] buffer = new byte[1024];
             int bytes;
 
             while (true){
